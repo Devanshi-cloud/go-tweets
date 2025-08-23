92e0f49c358f3108ec0e041f1f0e07aececde030
Devanshi Jaiswal <devanshishs@gmail.com>
2025-08-23 20:29:13 +0530

    changed StorePost query

--- internal/repository/post/store_post.go
+++ internal/repository/post/store_post.go
@@ -6,9 +6,9 @@ import (
 )
 
 func (r *postRepository) StorePost(ctx context.Context, model *model.PostModel) (int64, error) {
-	query := `INSERT INTO posts (user_id, content, created_at, updated_at) 
-	VALUES (?, ?, ?, ?)`
-	result, err := r.db.ExecContext(ctx, query, model.UserID, model.Content, model.CreatedAt, model.UpdatedAt)
+	query := `INSERT INTO posts (user_id, title, content, created_at, updated_at) 
+	VALUES (?, ?, ?, ?, ?)`
+	result, err := r.db.ExecContext(ctx, query, model.UserID, model.Title, model.Content, model.CreatedAt, model.UpdatedAt)
 	if err != nil {
 		return 0, err
 	}
